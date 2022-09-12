77f20aaa5097c623646832345b4cfadb37fa0100
Amith Condori Mamani <amithcondori123@gmail.com>
2022-09-12 00:55:33 -0400

    posts

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\PostController;
+
+Route::controller(PageController::class)->group(function () {
+    Route::get('/', 'home')->name('home');
+
+    Route::get('/blog', 'blog')->name('blog');
+
+    Route::get('/blog/{post:slug}', 'post')->name('post');
+});
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+Route::resource('posts', PostController::class)->except(['show'])->middleware(['auth']);
+
+require __DIR__.'/auth.php';
